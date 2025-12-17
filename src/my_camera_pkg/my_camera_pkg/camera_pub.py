703b5743b384aae4cd95056c336b861777ed1045
NguyenDinh <nguyenpazo123@gmail.com>
2025-12-17 16:54:08 +0700

    sda

--- src/my_camera_pkg/my_camera_pkg/camera_pub.py
+++ src/my_camera_pkg/my_camera_pkg/camera_pub.py
@@ -1,4 +1,4 @@
-import rclpy
+"""import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
@@ -34,5 +34,67 @@ def main(args = None):
         node.destroy_node()
         rclpy.shutdown()
 
+if __name__ == '__main__':
+    main()
+    """
+
+
+import rclpy  # ROS 2 Python client library
+from rclpy.node import Node    #Base class for ROS 2 nodes
+from sensor_msgs.msg import Image      #Message type for images
+from cv_bridge import CvBridge  #Bridge between ROS and OpenCV
+import cv2  #OpenCV library for image processing
+
+class CameraPublisher(Node):
+    def __init__(self):
+        super().__init__('camera_publisher')    #name of the node
+        # open the default camera
+        self.cap = cv2.VideoCapture(0)
+        # Check if the camera opened successfully
+        if not self.cap.isOpened():
+            self.get_logger().error('Cannot open camera')
+            raise Exception('Failed to open camera')
+        
+        # set resolution
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH,640)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
+        
+        self.bridge = CvBridge()    #translation between ROS and OpenCV
+        self.publisher = self.create_publisher(Image, '/camera/image_raw', 10)   #create publisher to publish images to /camera/image_raw topic
+
+        # timer 30FPS
+        self.timer = self.create_timer(1.0/30.0, self.timer_callback)
+        self.get_logger().info('Camera publisher staerted!')
+
+    def timer_callback(self):
+        # read frame from camera
+        ret, frame = self.cap.read()
+        if not ret:
+            self.get_logger().error('Failed to capture image')
+            return
+        else:
+            # convert OpenCV image to ROS Image message
+            msg = self.bridge.cv2_to_imgmsg(frame, encoding = 'bgr8')
+            msg.header.stamp = self.get_clock().now().to_msg()
+            msg.header.frame_id = 'camera_frame'
+
+            self.publisher.publish(msg)
+            self.get_logger().info('Captured image frame')
+
+    def __del__(self):
+        # release the camera when the node is destroyed
+        if hasattr(self, 'cap') and self.cap.isOpened():
+            self.cap.release()
+
+def main(args = None):
+    rclpy.init(args=args)
+    node = CameraPublisher()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
 if __name__ == '__main__':
     main()
--- src/my_camera_pkg/my_camera_pkg/camera_sub.py
+++ src/my_camera_pkg/my_camera_pkg/camera_sub.py
@@ -1,34 +1,49 @@
-import rclpy
-from rclpy.node import Node
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
-import cv2
+import rclpy  # ROS 2 Python client library
+from rclpy.node import Node    #Base class for ROS 2 nodes
+from sensor_msgs.msg import Image      #Message type for images
+from cv_bridge import CvBridge  #Bridge between ROS and OpenCV
+import cv2  #OpenCV library for image processing
 
 class CameraSubscriber(Node):
     def __init__(self):
-        super().__init__('camera_subscriber')
-        self.bridge = CvBridge()
+        super().__init__('camera_subscriber')   #name of the node
+        self.bridge = CvBridge()    #translation between ROS and OpenCV
+        # Create a subscription to the /camera/image_raw topic
         self.subscription = self.create_subscription(
-            Image,
-            '/camera/image_raw',
-            self.image_callback,
-            10
+            Image,    #message type
+            '/camera/image_raw',    #topic name
+            self.image_callback,   #callback function, called when a message is received
+            10  #queue size
+            
         )
         self.get_logger().info('Camera subscriber started!')
 
     def image_callback(self, msg):
+        """
+        Callback function to handle incoming image messages.
+        args:
+            msg (Image): The incoming image message.
+        """
         try:
-            cv_image = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
+            self.get_logger().info('Received image frame')
+            # Convert ROS Image message to OpenCV image
+            cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+            # get info about the image
+            height, width, channels = cv_image.shape
+            # Overlay image info on the image
+            text = f'Size: {width}x{height} |Frame: {msg.header.stamp.sec}.{msg.header.stamp.nanosec}'
+            cv2.putText(cv_image, text, (10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255, 0),2 )
             cv2.imshow('Camera', cv_image)
             cv2.waitKey(1)
         except Exception as e:
-            self.get_logger.error(f'Error: {e}')
+            self.get_logger.error('Error: '+ str(e))
 
 
 def main(args = None):
     rclpy.init(args=args)
     node = CameraSubscriber()
     try:
+        # Keep the node running to listen for incoming messages
         rclpy.spin(node)
 
     except KeyboardInterrupt:
